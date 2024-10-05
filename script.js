a7199cc9949fdd246d0b5c3f4b74e743222690dd
jikofuku <fukujikoo@gmail.com>
2024-10-05 09:35:00 +0545

    aaa TESTING
    aaa

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
- type="text/javascript" src="https://jiko-fuku.myspreadshop.com/shopfiles/shopclient/shopclient.nocache.js"></
+// Spreadshop configuration
+var spread_shop_config = {
+    shopName: 'jiko-fuku',
+    locale: 'us_US',
+    prefix: 'https://jiko-fuku.myspreadshop.com',
+    baseId: 'myShop',
+    usePushState: true,
+    updateMetadata: true,
+};
 
- window.onload = function() {
-const loader = document.getElementById('loader');
-loader.style.display = 'none';  // Hide loader once the page is fully loaded
- };
+// Script to hide loader after content is rendered
+window.onload = function() {
+    const loader = document.getElementById('loader');
+    loader.style.display = 'none';  // Hide loader once the page is fully loaded
+};
