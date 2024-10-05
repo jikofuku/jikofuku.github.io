44fb0d1f2ed64a42e885f320c7cfd24a553c3e67
jikofuku <fukujikoo@gmail.com>
2024-10-05 09:19:13 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+ type="text/javascript" src="https://jiko-fuku.myspreadshop.com/shopfiles/shopclient/shopclient.nocache.js"></
 
+ window.onload = function() {
+const loader = document.getElementById('loader');
+loader.style.display = 'none';  // Hide loader once the page is fully loaded
+ };
