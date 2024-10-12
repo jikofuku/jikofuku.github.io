1f0389e0c10810f208562bd4066adecf888118dc
jikofuku <fukujikoo@gmail.com>
2024-10-12 14:25:58 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-var spread_shop_config = {
-    shopName: 'jiko-fuku',
-    locale: 'us_US',
-    prefix: 'https://jiko-fuku.myspreadshop.com',
-    baseId: 'myShop',
-    usePushState: true,
-    updateMetadata: false
-};
-window.onload = function() {
-    const loader = document.getElementById('loader');
-    loader.style.display = 'none';
-    document.title = "Jiko Fuku";
-};
+var spread_shop_config={shopName:"jiko-fuku",locale:"us_US",prefix:"https://jiko-fuku.myspreadshop.com",baseId:"myShop",usePushState:!0,updateMetadata:!1};window.onload=function(){document.getElementById("loader").style.display="none",document.title="Jiko Fuku"};
