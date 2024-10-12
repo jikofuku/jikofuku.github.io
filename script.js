5760f420a47555fd264d8ea6cb6f8177a61dee36
jikofuku <fukujikoo@gmail.com>
2024-10-12 16:11:31 +0545

    a

--- script.js
+++ script.js
@@ -4,13 +4,8 @@ var spread_shop_config = {
     prefix: 'https://jiko-fuku.myspreadshop.com',
     baseId: 'myShop',
     usePushState: true,
-    updateMetadata: false // Ensure Spreadshirt doesn't override your metadata
+    updateMetadata: false
 };
-
 window.onload = function() {
-    const loader = document.getElementById('loader');
-    loader.style.display = 'none'; // Hide loader once the page is fully loaded
-
-    // Re-set the title after Spreadshop has loaded
-    document.title = "Jiko Fuku"; // Your custom title
+    document.title = "Jiko Fuku";  // Your custom title
 };
