e30f687a7b1dbab2637eaa86b2bd072ef28e4208
jikofuku <fukujikoo@gmail.com>
2024-10-12 16:18:04 +0545

    a

---
+++ config.js
@@ -0,0 +1,9 @@
+// spreadshop-config.js
+var spread_shop_config = {
+    shopName: 'jiko-fuku',
+    locale: 'us_US',
+    prefix: 'https://jiko-fuku.myspreadshop.com',
+    baseId: 'myShop',
+    usePushState: true,
+    updateMetadata: false // Ensure Spreadshirt doesn't override your metadata
+};
